fc2f8de5831e88cf8d2ab208ed29f0230942d6a4
Thyall <thyall96@gmail.com>
2021-04-05 16:58:59 -0300

    vcs são lixos

---
+++ webscrapper/src/main/java/br/imd/ufrn/webscrapper/App.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.webscrapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
